dee4aa9a92b0bac0dc6f9f875144be31e925a829
omniotosho <toloruntotosho@gmail.com>
2024-08-03 15:40:13 +0100

    URI Hot Fix

--- core/urls.py
+++ core/urls.py
@@ -13,9 +13,9 @@ urlpatterns = (
     path("api/v1/", include(router.urls)),
     path("core/User/", views.UserListView.as_view(), name="core_User_list"),
     path("core/User/create/", views.UserCreateView.as_view(), name="core_User_create"),
-    path("core/User/detail/<int:pk>/", views.UserDetailView.as_view(), name="core_User_detail"),
-    path("core/User/update/<int:pk>/", views.UserUpdateView.as_view(), name="core_User_update"),
-    path("core/User/delete/<int:pk>/", views.UserDeleteView.as_view(), name="core_User_delete"),
+    path("core/User/detail/<str:pk>/", views.UserDetailView.as_view(), name="core_User_detail"),
+    path("core/User/update/<str:pk>/", views.UserUpdateView.as_view(), name="core_User_update"),
+    path("core/User/delete/<str:pk>/", views.UserDeleteView.as_view(), name="core_User_delete"),
 
     path("core/htmx/User/", htmx.HTMXUserListView.as_view(), name="core_User_htmx_list"),
     path("core/htmx/User/create/", htmx.HTMXUserCreateView.as_view(), name="core_User_htmx_create"),
